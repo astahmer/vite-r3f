65ac76dbffcc2571b327a611060e1b7bf0cc76f9
Alexandre Stahmer <47224540+astahmer@users.noreply.github.com>
2021-09-13 20:17:30 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { ChakraProvider, extendTheme, Flex } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "./App.css";
+import { Demo } from "./components/Demo";
+
+const queryClient = new QueryClient();
+
+const theme = extendTheme({ config: { initialColorMode: "light" } });
+
+function App() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider theme={theme}>
+                <Flex direction="column" boxSize="100%">
+                    <Demo />
+                </Flex>
+            </ChakraProvider>
+        </QueryClientProvider>
+    );
+}
+
+export default App;
