b8cbf28efd9c5276cf56d068cdbc49a7326f9b09
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-09-13 20:40:15 +0200

    feat: add CameraControls

--- src/components/AppCanvas.tsx
+++ src/components/AppCanvas.tsx
@@ -1,11 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 
+import { CameraControls } from "./CameraControls";
+
 export const AppCanvas = () => {
     return (
         <Canvas orthographic gl={{ antialias: false }} camera={{ position: [0, 20, 0], up: [0, -1, 0], zoom: 50 }}>
             <axesHelper />
             <Suspense fallback={null}>{/* <Spritesheet /> */}</Suspense>
+            <CameraControls />
         </Canvas>
     );
 };
